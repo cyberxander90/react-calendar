f4dae53d48dd652533c460fd87139514de624c4a
Alexander Leyva Caro <cyberxander90@gmail.com>
2020-05-10 01:09:40 -0300

    normalize css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import './normalize.css';
+import App from 'src/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
